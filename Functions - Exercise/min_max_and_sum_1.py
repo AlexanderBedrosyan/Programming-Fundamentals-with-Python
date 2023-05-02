6387899315902a969923b07f04942dad35036dca
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-05-02 20:25:15 +0300

    Solutions for Functions - Exercise
    Solutions for Functions - Exercise

---
+++ Functions - Exercise/min_max_and_sum_1.py
@@ -0,0 +1,6 @@
+numbers = input().split()
+numbers = [int(ch) for ch in numbers]
+
+print(f"The minimum number is {min(numbers)}")
+print(f"The maximum number is {max(numbers)}")
+print(f"The sum number is: {sum(numbers)}")
