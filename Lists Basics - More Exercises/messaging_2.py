193dba484a781ea2ef8a9b9471e4787afe2bf8b9
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-06-02 11:57:21 +0300

    Solutions for Lists Basics - More Exercises
    Solutions for Lists Basics - More Exercises

---
+++ Lists Basics - More Exercises/messaging_2.py
@@ -0,0 +1,27 @@
+def finding_new_word(index_numbers_list, word_lsit):
+    new_list = []
+    for i in range(len(index_numbers_list)):
+        current_list = []
+        while index_numbers_list[i] > 0:
+            current_list.append(index_numbers_list[i] % 10)
+            index_numbers_list[i] = (index_numbers_list[i] - index_numbers_list[i] % 10) // 10
+
+        amount_current_list = sum(current_list)
+
+        while amount_current_list >= len(words_list):
+            amount_current_list -= len(words_list)
+
+        new_list.append(words_list[amount_current_list])
+        words_list.remove(words_list[amount_current_list])
+        amount_current_list = 0
+
+    return new_list
+
+index_numbers = input()
+words = input()
+
+index_numbers_list = [int(ch) for ch in index_numbers.strip().split()]
+words_list = [str(items) for items in words.strip()]
+new_list = finding_new_word(index_numbers_list, words_list)
+
+print(*new_list, sep='')
